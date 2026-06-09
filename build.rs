746dd27c2024d173ce74545822337c6bccb1f24d
Uladzislau Darazhei <uladzislau.darazhei@gmail.com>
2026-06-09 19:27:10 +0300

    feat: add desktop file for maccy-kde
    - Add maccy-kde.desktop
    
    - Update build.rs to copy desktop file

--- build.rs
+++ build.rs
@@ -1,3 +1,13 @@
 fn main() {
     slint_build::compile("ui/menu.slint").unwrap();
+    
+    #[cfg(target_os = "linux")]
+    {
+        // Copy desktop file to target
+        println!("cargo:rerun-if-changed=maccy-kde.desktop");
+        let out_dir = std::env::var("OUT_DIR").unwrap();
+        let src = std::path::Path::new("maccy-kde.desktop");
+        let dst = std::path::Path::new(&out_dir).join("maccy-kde.desktop");
+        std::fs::copy(src, dst).unwrap();
+    }
 }
